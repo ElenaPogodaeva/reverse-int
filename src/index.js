5350dba81baaa399db6d918acca2e1b4275e9000
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-08-13 15:10:35 +0300

    Implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+	let reversed = 0;
+	if (n < 0) {
+		n = -n;
+	} 
+	while(n) {
+	  reversed = reversed * 10 + n % 10;
+	  n = Math.floor(n / 10);
+	}
+	return reversed;
 }
